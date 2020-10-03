6e3d02b8945863e16d97d777db4b7efafd5f9625
adaboh <kweada@gmail.com>
2020-10-02 21:36:55 -0400

    completed Random quote generator day3

---
+++ scripts/day3.js
@@ -0,0 +1,53 @@
+const btn = document.getElementById("btn");
+
+var quotes = [
+  {
+    name: "Kwame Nkrumah",
+    quote:
+      "I am not African because I was born in Africa but because Africa was born in me",
+  },
+  {
+    name: "Jurgen Klopp",
+    quote:
+      "It needs time. Nobody wants to hear it, but that's the truth: if you want to have success in the future, you have to be ready to work now",
+  },
+  {
+    name: "Aristotle",
+    quote: "The energy of the mind is the essence of life",
+  },
+  {
+    name: "Fyodor Dostoyevsky",
+    quote:
+      "The mystery of human existence lies not in just staying alive, but in finding something to live for",
+  },
+];
+
+function random(value) {
+  return Math.floor(Math.random() * value);
+}
+
+function generateQuote() {
+  let quoteMsg = quotes[random(quotes.length)];
+  const blockQuoteText = document.querySelector(".blockquote-text");
+  blockQuoteText.textContent = quoteMsg.quote;
+
+  const authorName = document.getElementById("author-name");
+  authorName.textContent = quoteMsg.name;
+  const authorNameLength = quoteMsg.name.length;
+  console.log(authorNameLength);
+
+  let blockquoteAuthor = document.querySelector(".blockquote-author");
+
+  if (authorNameLength > 16) {
+    blockquoteAuthor.classList.remove("blockquote-author70");
+    blockquoteAuthor.classList.remove("blockquote-author80");
+  } else if (authorNameLength <= 10) {
+    blockquoteAuthor.classList.remove("blockquote-author70");
+    blockquoteAuthor.classList.add("blockquote-author80");
+  } else if (authorNameLength > 11) {
+    blockquoteAuthor.classList.remove("blockquote-author80");
+    blockquoteAuthor.classList.add("blockquote-author70");
+  }
+}
+
+btn.addEventListener("click", generateQuote);
