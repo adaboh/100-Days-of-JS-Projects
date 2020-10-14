23f1aaca3d32426ca09ec92645a220075372df12
adaboh <kweada@gmail.com>
2020-10-14 00:51:37 -0400

    completed day6

--- scripts/day5.js
+++ scripts/day5.js
@@ -1,28 +1,51 @@
-(function () {
-  const counter = document.querySelectorAll(".btn");
-  let count = 0;
+/* 
+solution 1
+ */
 
-  counter.forEach(function (btn) {
-    btn.addEventListener("click", function () {
-      if (btn.classList.contains("add")) {
-        count++;
-      } else if (btn.classList.contains("low")) {
-        count--;
-      } else if (btn.classList.contains("reset")) {
-        count = 0;
+/* const btns = document.querySelectorAll(".btn")
+const imgSrc = document.querySelector("#col-img")
+
+let randNo, lastRandNo;
+
+btns.forEach(function(btn){
+  btn.addEventListener("click", function(){
+    while(randNo === lastRandNo) {  
+    //  prevent random number generation duplication. but not efficient and use more cpu and memory 
+    //  https://codereview.stackexchange.com/questions/62018/prevent-repetitive-random-numbers
+    
+      randNo = Math.floor(Math.random()* 7 +1);
       }
-      //set count No text to count
-      let countNo = document.querySelector(".counter-no");
+      lastRandNo = randNo
+    imgSrc.src = "images/image-" + randNo + ".jpg"
+  })
+}) */
 
-      countNo.textContent = count;
+/* 
+SOLUTION 2
+*/
 
-      if (count > 0) {
-        countNo.style.color = "green";
-      } else if (count < 0) {
-        countNo.style.color = "red";
-      } else {
-        countNo.style.color = "#000";
-      }
-    });
-  });
-})();
+const btns = document.querySelectorAll(".btn")
+const imgSrc = document.querySelector("#col-img")
+let counter =0;
+let picNos = [1,2,3,4,5,6,7]
+
+btns.forEach(function(btn){
+btn.addEventListener("click", function(e){
+  if (btn.classList.contains("btn-left")){
+    counter--;
+    if(counter < 0) {
+      counter = picNos.length -1
+    }
+    imgSrc.src = "images/image-" + picNos[counter] + ".jpg"
+  };
+
+  if(btn.classList.contains("btn-right")){
+    counter++
+    if(counter > picNos.length -1) {
+      counter =0
+      console.log("counter is 0")
+    }
+    imgSrc.src = "images/image-" + picNos[counter]+ ".jpg"
+  }
+})
+})
