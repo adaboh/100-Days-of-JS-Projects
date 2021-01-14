5aa5631ebcac97ceca1292bf860e64c38a1cbb06
adaboh <kweada@gmail.com>
2021-01-14 06:13:23 -0500

    rename image files

--- scripts/day1.js
+++ scripts/day1.js
@@ -8,7 +8,7 @@ const btn = document.getElementById("btn");
 function random(value) {
   return Math.floor(Math.random() * value);
 }
-ja
+
 btn.addEventListener("click", function (e) {
   let generateColor = `rgb(${random(256)}, ${random(256)}, ${random(256)})`;
   body.style.backgroundColor = generateColor;
@@ -17,15 +17,16 @@ btn.addEventListener("click", function (e) {
 const body = document.querySelector("body");
 const btn = document.getElementById("btn");
 
-colors = ["red", "cyan", "orange", "green", "pink", "turquoise"];
+const colors = ["red", "cyan", "orange", "green", "pink", "turquoise"];
 
-body.style.backgroundColor = "cyan";
+//! testing js styling of bg
+body.style.backgroundColor = "steelblue";
 
 function changeColor() {
-  return colors;
+  return colors; // not used in the event listener below
 }
 
 btn.addEventListener("click", function () {
-  const randomIndex = Math.floor(Math.random() * colors.length);
+  randomIndex = Math.floor(Math.random() * colors.length);
   body.style.backgroundColor = colors[randomIndex];
 });
